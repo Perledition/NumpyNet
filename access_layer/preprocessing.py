build: backpropagation, fix: preprocessing normalization, weightsxavier init

access_layer/preprocessing.py
@@ -57,6 +57,7 @@ class PreProcessing:
 
         array_data = data[b'data']
         labels = data[b'labels']
+        print()
 
         # split the data into three channels and transform its shape to 32x32 image - cut is at 1024 columns
         # each row contains one training sample
@@ -76,10 +77,29 @@ class PreProcessing:
             rgbArray[:, :, 2] = sample[2048:3072].reshape((32, 32))
 
             # add image and label to data storage
-            self.x.append(rgbArray)
-            self.y.append(labels[0])
+            self.x.append(rgbArray/255)
+            self.y.append(labels[row])
 
         return True
+
+    def encode_labels(self):
+        # get unique classes
+        classes = list(set(self.y))
+        encoded_labels = list()
+
+        for label in self.y:
+
+            # create list with classes size 0's
+            label_data = [0 for _ in classes]
+
+            # replace 0 with 1 at index of label class
+            label_data[classes.index(label)] = 1
+
+            # add sample to collection list
+            encoded_labels.append(label_data)
+
+        # return nested list with results as array
+        return np.array(encoded_labels).transpose()
 
     @staticmethod
     def split_channels(image):
@@ -96,4 +116,4 @@ class PreProcessing:
             rgb_shape.append(self.split_channels(sample))
 
         self.x = rgb_shape
-        return self.x, self.y
+        return self.x, self.encode_labels()

layer/network_layer.py
@@ -91,11 +91,14 @@ class ActivationLayer:
 
 class DenseLayer(ActivationLayer):
 
-    def __init__(self, nodes=2, learning_rate=0.1, activation_type='relu', layer_id=None):
+    def __init__(self, nodes=10, output_layer=False, activation_type='relu', layer_id=None, learning_rate=0.1):
         super().__init__(activation_type)
         self.learning_rate = learning_rate
         self.nodes = nodes
         self.new_layer = True
+        self.output_layer = output_layer
+        self.input = None
+        self.x = None
         self.weights, self.bias = None, None
 
         # assign random layer id code to the layer to make temp files and results locatable
@@ -108,16 +111,38 @@ class DenseLayer(ActivationLayer):
             self.new_layer = False
 
     def initialize_weights(self, input_dim):
-        weights = np.random.rand(self.nodes, input_dim) * 0.01
+        weights = np.random.rand(self.nodes, input_dim) * math.sqrt((2/input_dim))
         bias = np.zeros((self.nodes, 1))
         return weights, bias
 
-    def forward(self, x):
+    def forward_propagation(self, x):
         z = np.dot(self.weights, x.transpose()) + self.bias
-        return self.activation[self.activation_type](z)
+        return self.activation[self.activation_type](z).transpose()
 
-    def backward(self):
-        pass
+    def backward_propagation(self, x, y):
+        if self.output_layer:
+            # calculate the error
+            dZ = x - y
+
+            # create d of all weights
+            dw = (1/x.shape[1]) * np.dot(dZ, self.input.T)
+            db = (1/x.shape[1]) * np.sum(dZ, axis=1, keepdims=True)
+
+        else:
+            # calculate the error
+            dZ = np.multiply(np.dot(self.weights, x), 1 - np.power(self.x, 2))
+
+            # create d of all weights
+            dw = (1/x.shape[1]) * np.dot(dZ, self.input.T)
+            db = (1/x.shape[1]) * np.sum(dZ, axis=1, keepdims=True)
+
+        # Multiply the gradients by learning rate
+        self.weights = self.weights - self.learning_rate * dw
+        self.bias = self.bias - self.learning_rate * db
+
+        # overwrite both temp files to keep the last changes
+        self.write_weights(self.weights)
+        self.write_weights(self.bias, extension='bias')
 
     def load_weights(self):
 
@@ -140,6 +165,7 @@ class DenseLayer(ActivationLayer):
         np.save(path, weights)
 
     def assign(self, x):
+        self.input = x
 
         # get the number of samples, how many filters are and how many features coming in for each node
         samples, features = x.shape
@@ -148,15 +174,18 @@ class DenseLayer(ActivationLayer):
         # initialize new weights in case of a new initialized layer without any weights created yet
         if self.new_layer:
             self.weights, self.bias = self.initialize_weights(features)
-            self.write_weights(self.weights)
-            self.write_weights(self.bias, 'bias')
+
+            # self.write_weights(self.weights)
+            # self.write_weights(self.bias, 'bias')
 
         # load weights from file in case the Layer was created and used before
         else:
             self.weights = self.load_weights()
             self.bias = self.load_bias()
 
-        return self.forward(x)
+        # save the results of forward propagation for this layer for the backwards propagation
+        self.x = self.forward_propagation(x)
+        return self.x
 
 
 class DropoutLayer:
@@ -174,7 +203,7 @@ class DropoutLayer:
 
 class ConvolutionalLayer:
 
-    def __init__(self, border_mode='valid', filter_size=3, filter_count=3, layer_id=None):
+    def __init__(self, border_mode='valid', filter_size=3, filter_count=5, layer_id=None):
 
         # make border mode a global variable of the class and do quality check beforehand
         # With border mode "valid"/"zero" you get an output that is smaller than the input because the convolution is
@@ -260,7 +289,7 @@ class ConvolutionalLayer:
                    for _ in range(self.filter_count)]
 
         # save a temp file in which the filters will be saved for later usage
-        self.write_filters(kernels)
+        # self.write_filters(kernels)
         return kernels
 
     def load_filters(self):
@@ -324,12 +353,6 @@ class ConvolutionalLayer:
             else:
                 # return kernel_count x kernel_count, 3, 30, 30
                 conv_features = np.append(conv_features, [kernel_run_features], axis=0)
-
-            # status update
-            if nr == len(x) - 1:
-                print(f'Conv: {"="*100 + ">"}|{nr}/{len(x)}')
-            else:
-                print(f'Conv: {"="*(100 - int((1 - (nr + 1/len(x)))*100))+ ">" +  " "*int((1 - (nr + 1/len(x)))*100)}|{nr+1}/{len(x)}')
 
         return conv_features
 

layer/processing.py
@@ -1,21 +1,97 @@
 # import standard modules
+import random as rm
+import datetime as dt
+
+# import third party modules
 import numpy as np
 
 
 class Sequential:
 
-    def __init__(self, layers, epochs=100):
+    def __init__(self, layers, epochs=20, batch_size=100):
         self.layers = layers
         self.epochs = epochs
-        self.crossentropy = None
+        self.batch_size = batch_size
 
-    def print_process(self):
-        pass
+    @staticmethod
+    def status_update(cost, batch_nr, batch_size, epoch, time):
+        if batch_nr == batch_size - 1:
+            print(f'epoch {epoch}: {"=" * 100 + ">"}|{batch_nr / batch_size} | cost: {cost} | duration: {time}')
+        else:
+            print(
+                f'epoch {epoch}: {"=" * (100 - int((1 - ((batch_nr + 1) / batch_size)) * 100)) + ">" + " " * int((1 - ((batch_nr + 1) / batch_size)) * 100)}|{(batch_nr + 1) / batch_size}% | cost: {cost} | duration: {time}')
 
-    def batch_generator(self):
-        pass
+    def batch_generator(self, x, y):
+        # shuffle x and y together to make sure that all batches contain more than one class
+        c = list(zip(x, list(y.transpose())))
+        rm.shuffle(c)
 
-    def train(self, x):
-        for layer in self.layers:
-            x = layer.assign(x)
+        # initialize starting parameters for creating batch sizes
+        batches = list()
+        batches_count = int(len(x) / self.batch_size)
+        start = 0
+        end = self.batch_size
 
+        # start splitting samples into batches
+        for i in range(1, batches_count + 1):
+            if i + 1 < batches_count:
+                batches.append(c[start:end])
+            else:
+                batches.append(c[start:])
+
+            start += self.batch_size
+            end += self.batch_size
+
+        return batches
+
+    @staticmethod
+    def calculate_cost(x, y):
+        #print(np.multiply(np.log(x).transpose(), y))
+        #print(np.multiply((1 - y), np.log(1 - x).transpose()))
+        logprobs = np.multiply(np.log(x).transpose(), y) + np.multiply((1 - y), np.log(1 - x).transpose())
+        cost = -np.sum(logprobs) / x.shape[1]
+        return cost
+
+    def batch_processing(self, x, y):
+
+        x_return = x
+        # forward propagation for all layers
+        for i, layer in enumerate(self.layers):
+            x_return = layer.assign(x_return)
+            # print(i, x_return.shape)
+
+        cost = self.calculate_cost(x_return, y)
+
+        # backward propagation for all layers
+        for i in range(len(self.layers)):
+            try:
+                x_return = self.layers[-i].backward_propagation(x_return, y)
+            except Exception as e:
+                # print(e)
+                continue
+
+        return cost
+
+    def train(self, x, y):
+        print('create batches for training process...')
+        batches = self.batch_generator(x, y)
+        print(f'{len(batches)} were created with samples size of {self.batch_size} each..')
+        print('start training...')
+
+        for epoch in range(self.epochs):
+            epoch_start = dt.datetime.now()
+            for ix, batch in enumerate(batches):
+                batch_start = dt.datetime.now()
+                # decode batch into usable x and y since it is through the batch generator in a list
+                tx, ty = zip(*batch)
+                ty = np.array(ty).transpose()
+                # print('x in:', tx)
+                # print('x in:', len(tx), tx[0].shape)
+
+                cost = self.batch_processing(tx, ty)
+
+                # status update
+                duration = str(dt.datetime.now() - batch_start)
+                self.status_update(cost, ix, len(batches), epoch, duration)
+
+            print(f'epoch {epoch}: {dt.datetime.now() - epoch_start}')